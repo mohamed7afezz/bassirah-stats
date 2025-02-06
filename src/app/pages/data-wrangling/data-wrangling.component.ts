ee85017263956215ccff6dd4366584a6f51991a9
Osama Zaki <59180276+osamazak@users.noreply.github.com>
2025-02-06 20:48:44 +0200

    add Iframe

--- src/app/pages/data-wrangling/data-wrangling.component.ts
+++ src/app/pages/data-wrangling/data-wrangling.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-wrangling',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './data-wrangling.component.html',
   styleUrl: './data-wrangling.component.scss'
 })
-export class DataWranglingComponent {
+export class DataWranglingComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const iframe = document.getElementById('stat-metabase') as HTMLIFrameElement;
+
+    if (iframe) {
+      iframe.onload = () => {
+        const iframeDoc = iframe.contentDocument || iframe.contentWindow?.document;
+
+        if (iframeDoc) {
+          const script = iframeDoc.createElement("script");
+          script.textContent = `
+            window.onload = function() {
+              setTimeout(function() {
+                document.querySelector('input[name="username"]').value = "admin@bassirah.local";
+                document.querySelector('input[name="password"]').value = "UO678gf4oui0yQEt"; 
+                document.querySelector('button[type="submit"]').click();
+              }, 10000);
+                
+            }
+          `;
+          iframeDoc.head.appendChild(script);
+        }
+      };
+    }
+  }
 }
