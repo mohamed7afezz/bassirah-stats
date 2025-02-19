96422b8245275d8268b5cb969cb1a0f8776ebe9d
Yehia <Yehia>
2025-02-20 00:28:52 +0200

    Add Workflow Diagram

--- src/app/pages/work-flow/work-flow.component.ts
+++ src/app/pages/work-flow/work-flow.component.ts
@@ -1,39 +1,30 @@
-import { Component } from '@angular/core';
-import { WorkflowDesignerModule } from '@optimajet/workflow-designer-angular';
+import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { WorkflowDiagramComponent } from '../workflow-diagram/workflow-diagram.component';
 
 @Component({
   selector: 'app-work-flow',
   standalone: true,
-  imports: [WorkflowDesignerModule],
+  imports: [WorkflowDiagramComponent],
   templateUrl: './work-flow.component.html',
   styleUrl: './work-flow.component.scss'
 })
-export class WorkFlowComponent {
-  schemeCode = 'SimpleWF';
-  processId = '';
-  designerConfig = {
-    apiurl: 'https://demo.workflowengine.io/Designer/API',
-    renderTo: 'wf-designer',
-    uploadFormId: 'wf-uploadFormId',
-    uploadFileId: 'wf-uploadFileId',
-    widthDiff: 0,
-    heightDiff: 0,
-    name: 'wfe',
-    language: 'en',
-    // customLocalization: [el]
-  };
-
-  url: string = "https://demo.workflowserver.io/?apanel=workflow&aid=BPMN2";
-  // Sanitize the URL
+export class WorkFlowComponent implements OnInit {
+  url: string = "https://demo.workflowserver.io/?apanel=workflow&aid=BPMN2?embedded=true";
   safeUrl: SafeResourceUrl;
 
   constructor(private sanitizer: DomSanitizer) {
     this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
 
-  // Handle workflow changes
-  onWorkflowChange(event: any) {
-    console.log('Workflow changed:', event);
+  ngOnInit(): void { }
+
+  // Handle iframe load event
+  onIframeLoad(): void {
+    const iframe = document.querySelector('iframe');
+    if (iframe) {
+      // Send a message to the iframe
+      iframe.contentWindow?.postMessage({ action: 'clickButton' }, '*');
+    }
   }
 }
